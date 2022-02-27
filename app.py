318ba2ad4c3c8c4d0312fd618f66a00a66c60ec2
Basavraj Chinagundi <75333151+basavraj-chinagundi@users.noreply.github.com>
2022-02-27 19:59:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.title('Swoonflix')
 @st.cache
 def get_data():
 	path = r'thisIsLast.csv'
-	f=pd.read_csv(path,encoding='utf-16')
+	f=pd.read_csv(path,encoding='utf-8-sig')
 	return f
 def set_data(f,c1,c2):
 	f=f.loc[((f["category"] == str(c1)) & (f["country"] == str(c2)))]
